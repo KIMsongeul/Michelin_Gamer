211899e0194241e4d0151bad8011e612a3dd1455
songeul <0754minjun@gmail.com>
2024-11-12 21:10:34 +0900

    RhythmBoss DJTable 움직임 추가(+개선필요)

---
+++ Assets/Scripts/RhythmBoss/DJTable.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DJTable : MonoBehaviour
+{
+    private float upMax = 0f;
+    private float downMax = -2.0f;git 
+    private float currentPosition;
+    private float direction = 3.0f;
+
+    private void Start()
+    {
+        currentPosition = transform.position.y;
+    }
+
+    private void Update()
+    {
+        currentPosition += Time.deltaTime * direction;
+        if (currentPosition >= upMax)
+        {
+            direction *= -1;
+            currentPosition = upMax;
+        }
+        else if (currentPosition <= downMax)
+        {
+            direction *= -1;
+            currentPosition = downMax;
+        }
+
+        transform.position = new Vector3(0, currentPosition, 0);
+    }
+}
